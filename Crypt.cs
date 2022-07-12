798aae9e8500d5c2d3cdcfdb5920f543a1f236ab
Kyle <kjloveless321@gmail.com>
2022-07-12 13:08:21 -0500

    Clean up Crypt unit, get rid of unneeded variable, fix null reference warnings.

--- Crypt.cs
+++ Crypt.cs
@@ -5,10 +5,9 @@ namespace ftw_msgr.Crypto;
 
 public class Crypt
 {
-    ECDiffieHellman localKey;
-    ECDiffieHellmanPublicKey localPublicKey;
+    private ECDiffieHellman localKey;
+    private ECDiffieHellmanPublicKey localPublicKey;
     public ECPoint localPublicKey_Q;
-    public ECDiffieHellman? RemoteKey { get; set; }
     public ECDiffieHellmanPublicKey? remotePublicKey { get; set; }
 
     public Crypt()
@@ -16,7 +15,6 @@ public class Crypt
         localKey = ECDiffieHellman.Create();
         localPublicKey = localKey.PublicKey;
         localPublicKey_Q = localPublicKey.ExportParameters().Q;
-        RemoteKey = ECDiffieHellman.Create();
     }
 
     public string DecryptMessage(string ciphertext)
@@ -30,29 +28,30 @@ public class Crypt
         byte[] ivData = new byte[ivLength];
         Array.Copy(data, ivData, ivLength);
 
-
-        var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
-        aes.Key = sumKey;
-        aes.IV = ivData;
-        using ICryptoTransform decryptor = aes.CreateDecryptor();
-        using MemoryStream ms = new MemoryStream();
-        
-        var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
-        cs.Write(data, ivLength, data.Length - ivLength);
-        cs.Close();
-        rawData = ms.ToArray();
-        return Encoding.UTF8.GetString(rawData);
-        
-        
+        if (remotePublicKey is not null)
+        {
+            var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
+            aes.Key = sumKey;
+            aes.IV = ivData;
+            using ICryptoTransform decryptor = aes.CreateDecryptor();
+            using MemoryStream ms = new MemoryStream();
+            
+            var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
+            cs.Write(data, ivLength, data.Length - ivLength);
+            cs.Close();
+            rawData = ms.ToArray();
+            return Encoding.UTF8.GetString(rawData);
+        }        
+        else 
+        {
+            return $"Unable to decrypt: {ciphertext} .\n No remote public key found.";
+        }
     }
 
     public T? EncryptMessage<T>(T rawMessage)
     {
-        if (typeof(T) == typeof(string))
+        if (remotePublicKey is not null)
         {
-            string? message = rawMessage as string;
-            if (message is null) { message = ""; }
-            byte[] rawData = Encoding.UTF8.GetBytes(message);
             var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
 
             using var aes = AesCng.Create();
@@ -64,41 +63,36 @@ public class Crypt
             using MemoryStream ms = new MemoryStream();
             
             var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-            ms.Write(aes.IV, 0, aes.IV.Length);
-            cs.Write(rawData, 0, rawData.Length);
-            cs.Close();
-            var data = ms.ToArray();
-            return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));
-                    
-                
             
-        }
-        else if (typeof(T) == typeof(byte[]))
-        {
-            char[]? chars = rawMessage as char[];
-            if (chars is null) { chars = Array.Empty<char>(); }
-            byte[] rawData = Encoding.UTF8.GetBytes(chars);
-            var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
+            if (typeof(T) == typeof(string))
+            {
+                string? message = rawMessage as string;
+                if (message is null) { message = ""; }
+                byte[] rawData = Encoding.UTF8.GetBytes(message);
+                
+                ms.Write(aes.IV, 0, aes.IV.Length);
+                cs.Write(rawData, 0, rawData.Length);
+                cs.Close();
+                var data = ms.ToArray();
+                return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));            
+            }
+            else if (typeof(T) == typeof(byte[]))
+            {
+                char[]? chars = rawMessage as char[];
+                if (chars is null) { chars = Array.Empty<char>(); }
+                byte[] rawData = Encoding.UTF8.GetBytes(chars);
 
-            using var aes = AesCng.Create();
-            
-            aes.Key = sumKey;
-            aes.GenerateIV();
-            using ICryptoTransform encryptor = aes.CreateEncryptor();
-            
-            using MemoryStream ms = new MemoryStream();
-            
-            CryptoStream? cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-            ms.Write(aes.IV, 0, aes.IV.Length);
-            cs.Write(rawData, 0, rawData.Length);
-            cs.Close();
-            byte[]? myBytes = ms.ToArray();
-            char[]? myChars = new char[myBytes.Length];
-            Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
-            return (T?)Convert.ChangeType(myChars, typeof(T));
-                    
-                
-            
+                ms.Write(aes.IV, 0, aes.IV.Length);
+                cs.Write(rawData, 0, rawData.Length);
+                cs.Close();
+                byte[]? myBytes = ms.ToArray();
+                char[]? myChars = new char[myBytes.Length];
+                Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
+                return (T?)Convert.ChangeType(myChars, typeof(T));            
+            } else
+            {
+                return default;
+            }
         } else
         {
             return default;
