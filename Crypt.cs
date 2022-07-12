654c6fa54224b279d0b55f843e517ff28b120886
Kyle <kjloveless321@gmail.com>
2022-07-12 12:53:20 -0500

    Switch to using declarations, added in C# 8 [https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-statement#example]

--- Crypt.cs
+++ Crypt.cs
@@ -24,26 +24,26 @@ public class Crypt
         byte[] data = System.Convert.FromBase64String(ciphertext);
         byte[] rawData;
 
-        using (var aes = AesCng.Create())
-        {
-            var ivLength = aes.BlockSize >> 3;
-            byte[] ivData = new byte[ivLength];
-            Array.Copy(data, ivData, ivLength);
+        using var aes = AesCng.Create();
+        
+        var ivLength = aes.BlockSize >> 3;
+        byte[] ivData = new byte[ivLength];
+        Array.Copy(data, ivData, ivLength);
 
 
-            var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
-            aes.Key = sumKey;
-            aes.IV = ivData;
-            using (ICryptoTransform decryptor = aes.CreateDecryptor())
-            using (MemoryStream ms = new MemoryStream())
-            {
-                var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
-                cs.Write(data, ivLength, data.Length - ivLength);
-                cs.Close();
-                rawData = ms.ToArray();
-                return Encoding.UTF8.GetString(rawData);
-            }
-        }
+        var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
+        aes.Key = sumKey;
+        aes.IV = ivData;
+        using ICryptoTransform decryptor = aes.CreateDecryptor();
+        using MemoryStream ms = new MemoryStream();
+        
+        var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
+        cs.Write(data, ivLength, data.Length - ivLength);
+        cs.Close();
+        rawData = ms.ToArray();
+        return Encoding.UTF8.GetString(rawData);
+        
+        
     }
 
     public T? EncryptMessage<T>(T rawMessage)
@@ -55,23 +55,23 @@ public class Crypt
             byte[] rawData = Encoding.UTF8.GetBytes(message);
             var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
 
-            using (var aes = AesCng.Create())
-            {
-                aes.Key = sumKey;
-                aes.GenerateIV();
-                using (ICryptoTransform encryptor = aes.CreateEncryptor())
-                {
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-                        ms.Write(aes.IV, 0, aes.IV.Length);
-                        cs.Write(rawData, 0, rawData.Length);
-                        cs.Close();
-                        var data = ms.ToArray();
-                        return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));
-                    }
-                }
-            }
+            using var aes = AesCng.Create();
+            
+            aes.Key = sumKey;
+            aes.GenerateIV();
+            using ICryptoTransform encryptor = aes.CreateEncryptor();
+            
+            using MemoryStream ms = new MemoryStream();
+            
+            var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            ms.Write(aes.IV, 0, aes.IV.Length);
+            cs.Write(rawData, 0, rawData.Length);
+            cs.Close();
+            var data = ms.ToArray();
+            return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));
+                    
+                
+            
         }
         else if (typeof(T) == typeof(byte[]))
         {
@@ -80,25 +80,25 @@ public class Crypt
             byte[] rawData = Encoding.UTF8.GetBytes(chars);
             var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
 
-            using (var aes = AesCng.Create())
-            {
-                aes.Key = sumKey;
-                aes.GenerateIV();
-                using (ICryptoTransform encryptor = aes.CreateEncryptor())
-                {
-                    using (MemoryStream ms = new MemoryStream())
-                    {
-                        CryptoStream? cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-                        ms.Write(aes.IV, 0, aes.IV.Length);
-                        cs.Write(rawData, 0, rawData.Length);
-                        cs.Close();
-                        byte[]? myBytes = ms.ToArray();
-                        char[]? myChars = new char[myBytes.Length];
-                        Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
-                        return (T?)Convert.ChangeType(myChars, typeof(T));
-                    }
-                }
-            }
+            using var aes = AesCng.Create();
+            
+            aes.Key = sumKey;
+            aes.GenerateIV();
+            using ICryptoTransform encryptor = aes.CreateEncryptor();
+            
+            using MemoryStream ms = new MemoryStream();
+            
+            CryptoStream? cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            ms.Write(aes.IV, 0, aes.IV.Length);
+            cs.Write(rawData, 0, rawData.Length);
+            cs.Close();
+            byte[]? myBytes = ms.ToArray();
+            char[]? myChars = new char[myBytes.Length];
+            Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
+            return (T?)Convert.ChangeType(myChars, typeof(T));
+                    
+                
+            
         } else
         {
             return default;
