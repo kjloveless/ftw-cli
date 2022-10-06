1c08d49ece6da6a86aaa15ed8969a9eb7ce3df9e
Kyle <kjloveless321@gmail.com>
2022-10-06 08:46:37 -0500

    Fix formatting.

--- Crypt.cs
+++ Crypt.cs
@@ -5,107 +5,109 @@ namespace ftw_msgr.Crypto;
 
 public class Crypt
 {
-    private ECDiffieHellman localKey;
-    private ECDiffieHellmanPublicKey localPublicKey;
-    public ECPoint localPublicKey_Q;
-    public ECDiffieHellmanPublicKey? remotePublicKey { get; set; }
+  private ECDiffieHellman localKey;
+  private ECDiffieHellmanPublicKey localPublicKey;
+  public ECPoint localPublicKey_Q;
+  public ECDiffieHellmanPublicKey? remotePublicKey { get; set; }
 
-    public Crypt()
+  public Crypt()
+  {
+    localKey = ECDiffieHellman.Create();
+    localPublicKey = localKey.PublicKey;
+    localPublicKey_Q = localPublicKey.ExportParameters().Q;
+  }
+
+  public string DecryptMessage(string ciphertext)
+  {
+    byte[] data = System.Convert.FromBase64String(ciphertext);
+    byte[] rawData;
+
+    using var aes = AesCng.Create();
+
+    var ivLength = aes.BlockSize >> 3;
+    byte[] ivData = new byte[ivLength];
+    Array.Copy(data, ivData, ivLength);
+
+    if (remotePublicKey is not null)
     {
-        localKey = ECDiffieHellman.Create();
-        localPublicKey = localKey.PublicKey;
-        localPublicKey_Q = localPublicKey.ExportParameters().Q;
-    }
+      var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
+      aes.Key = sumKey;
+      aes.IV = ivData;
+      using ICryptoTransform decryptor = aes.CreateDecryptor();
+      using MemoryStream ms = new MemoryStream();
 
-    public string DecryptMessage(string ciphertext)
+      var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
+      cs.Write(data, ivLength, data.Length - ivLength);
+      cs.Close();
+      rawData = ms.ToArray();
+      return Encoding.UTF8.GetString(rawData);
+    }
+    else
     {
-        byte[] data = System.Convert.FromBase64String(ciphertext);
-        byte[] rawData;
-
-        using var aes = AesCng.Create();
-        
-        var ivLength = aes.BlockSize >> 3;
-        byte[] ivData = new byte[ivLength];
-        Array.Copy(data, ivData, ivLength);
-
-        if (remotePublicKey is not null)
-        {
-            var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
-            aes.Key = sumKey;
-            aes.IV = ivData;
-            using ICryptoTransform decryptor = aes.CreateDecryptor();
-            using MemoryStream ms = new MemoryStream();
-            
-            var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write);
-            cs.Write(data, ivLength, data.Length - ivLength);
-            cs.Close();
-            rawData = ms.ToArray();
-            return Encoding.UTF8.GetString(rawData);
-        }        
-        else 
-        {
-            return $"Unable to decrypt: {ciphertext} .\n No remote public key found.";
-        }
+      return $"Unable to decrypt: {ciphertext} .\n No remote public key found.";
     }
+  }
 
-    public T? EncryptMessage<T>(T rawMessage)
+  public T? EncryptMessage<T>(T rawMessage)
+  {
+    if (remotePublicKey is not null)
     {
-        if (remotePublicKey is not null)
-        {
-            var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
+      var sumKey = localKey.DeriveKeyMaterial(remotePublicKey);
 
-            using var aes = AesCng.Create();
-            
-            aes.Key = sumKey;
-            aes.GenerateIV();
-            using ICryptoTransform encryptor = aes.CreateEncryptor();
-            
-            using MemoryStream ms = new MemoryStream();
-            
-            var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
-            
-            if (typeof(T) == typeof(string))
-            {
-                string? message = rawMessage as string;
-                if (message is null) { message = ""; }
-                byte[] rawData = Encoding.UTF8.GetBytes(message);
-                
-                ms.Write(aes.IV, 0, aes.IV.Length);
-                cs.Write(rawData, 0, rawData.Length);
-                cs.Close();
-                var data = ms.ToArray();
-                return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));            
-            }
-            else if (typeof(T) == typeof(byte[]))
-            {
-                char[]? chars = rawMessage as char[];
-                if (chars is null) { chars = Array.Empty<char>(); }
-                byte[] rawData = Encoding.UTF8.GetBytes(chars);
+      using var aes = AesCng.Create();
 
-                ms.Write(aes.IV, 0, aes.IV.Length);
-                cs.Write(rawData, 0, rawData.Length);
-                cs.Close();
-                byte[]? myBytes = ms.ToArray();
-                char[]? myChars = new char[myBytes.Length];
-                Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
-                return (T?)Convert.ChangeType(myChars, typeof(T));            
-            } else
-            {
-                return default;
-            }
-        } else
-        {
-            return default;
-        }
+      aes.Key = sumKey;
+      aes.GenerateIV();
+      using ICryptoTransform encryptor = aes.CreateEncryptor();
+
+      using MemoryStream ms = new MemoryStream();
+
+      var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+
+      if (typeof(T) == typeof(string))
+      {
+        string? message = rawMessage as string;
+        if (message is null) { message = ""; }
+        byte[] rawData = Encoding.UTF8.GetBytes(message);
+
+        ms.Write(aes.IV, 0, aes.IV.Length);
+        cs.Write(rawData, 0, rawData.Length);
+        cs.Close();
+        var data = ms.ToArray();
+        return (T?)Convert.ChangeType(Convert.ToBase64String(data), typeof(T));
+      }
+      else if (typeof(T) == typeof(byte[]))
+      {
+        char[]? chars = rawMessage as char[];
+        if (chars is null) { chars = Array.Empty<char>(); }
+        byte[] rawData = Encoding.UTF8.GetBytes(chars);
+
+        ms.Write(aes.IV, 0, aes.IV.Length);
+        cs.Write(rawData, 0, rawData.Length);
+        cs.Close();
+        byte[]? myBytes = ms.ToArray();
+        char[]? myChars = new char[myBytes.Length];
+        Convert.ToBase64CharArray(myBytes, 0, myBytes.Length, myChars, 0);
+        return (T?)Convert.ChangeType(myChars, typeof(T));
+      }
+      else
+      {
+        return default;
+      }
     }
-
-    public void InitRemotePublicKey(ECPoint myQ)
+    else
     {
-        var ecdh = ECDiffieHellman.Create();
-        ECParameters myECParams = new() { Q = myQ };
-        ECCurve curve = localKey.ExportParameters(false).Curve;
-        myECParams.Curve = curve;
-        ecdh.ImportParameters(myECParams);
-        remotePublicKey = ecdh.PublicKey;
+      return default;
     }
+  }
+
+  public void InitRemotePublicKey(ECPoint myQ)
+  {
+    var ecdh = ECDiffieHellman.Create();
+    ECParameters myECParams = new() { Q = myQ };
+    ECCurve curve = localKey.ExportParameters(false).Curve;
+    myECParams.Curve = curve;
+    ecdh.ImportParameters(myECParams);
+    remotePublicKey = ecdh.PublicKey;
+  }
 }
--- Program.cs
+++ Program.cs
@@ -4,31 +4,31 @@ using ftw_msgr.Connection;
 Base_Connection? msgrServer = null;
 
 string? arg;
-do  
+do
 {
-    Console.Write("client or server? ");            
-    arg = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(arg)) { arg = ""; }
+  Console.Write("client or server? ");
+  arg = Console.ReadLine();
+  if (string.IsNullOrWhiteSpace(arg)) { arg = ""; }
 } while (arg != "client" && arg != "server");
 
 switch (arg)
 {
-    case "server": 
-      msgrServer = new Server(); 
-      break;
-    case "client": 
-      msgrServer = new Client(); 
-      break;
+  case "server":
+    msgrServer = new Server();
+    break;
+  case "client":
+    msgrServer = new Client();
+    break;
 }
 
 
 string? text;
 
-while (true && msgrServer is not null) 
+while (true && msgrServer is not null)
 {
-    Console.Write("Send: ");
-    text = Console.ReadLine();
-    msgrServer.MsgHistory.Add($"Send: {text}");
-    if (text == null) break;
-    msgrServer.SendMsg(text);
+  Console.Write("Send: ");
+  text = Console.ReadLine();
+  msgrServer.MsgHistory.Add($"Send: {text}");
+  if (text == null) break;
+  msgrServer.SendMsg(text);
 };
